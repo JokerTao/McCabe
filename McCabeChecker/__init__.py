b1a0fda401f3ed6830f89ded31e70e6a34df9e18
JokerTao <33825077+JokerTao@users.noreply.github.com>
2021-11-02 15:48:22 +0800

    Create __init__.py

---
+++ McCabeChecker/__init__.py
@@ -0,0 +1,10 @@
+#encoding=utf-8
+
+"""
+#@version: 
+#@author: lipengtao
+#@contact: lipengtao@bjgoodwill.com
+#@software: PyCharm Community Edition
+#@file: __init__.py.py
+#@time: 21-11-2 下午1:55
+"""
